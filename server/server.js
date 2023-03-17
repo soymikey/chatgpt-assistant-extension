9edce38222d72998dd7a4ee993354a6e38f5f7c2
soymikey <soymikey0717@gmail.com>
2023-03-17 17:06:34 +0800

    更新getAnswer接口

--- server/server.js
+++ server/server.js
@@ -76,18 +76,14 @@ app.use(async ctx => {
             console.log('问题:==========================================',);
             console.log(questionContent);
             console.log("==========================================");
-            const res = await mockApi()
-            // const res = await api.sendMessage(questionContent)
+            // const res = await mockApi()
+            const res = await api.sendMessage(questionContent)
+            Question.create({ sub, content: questionContent })
             console.log("答案:", res.text);
             ctx.body = successModel(res)
         } catch (error) {
             console.log('获取答案Error:', error);
             ctx.body = errorModel(error)
-        } finally {
-            Question.create({
-                sub,
-                content: questionContent
-            })
         }
     }
 
