58742a1d102ae87772d59f2ed73c065ce2a66e67
soymikey <soymikey0717@gmail.com>
2023-03-18 15:40:24 +0800

    更新chatgpt api

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
     stripeAPI: 'sk_test_51Bq0KgHIArwfBLFuAX7eaDOiPgKILjlavaSlZA3pDhtgPpc44pagdCYbtL1riZlrNXJfERZWoiJ0cSKSrpNWF6sJ001BhtV4Zi',
-    chatGPTAPI: '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'
+    chatGPTAPI: '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'
 }
 export default config
